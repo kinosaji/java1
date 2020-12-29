62a234c2fa397353c730966dee7e6f808f9b49ca
kinosaji <75092257+kinosaji@users.noreply.github.com>
2020-12-29 12:07:10 +0900

    rename

--- study2020/src/study2020/MemoryCheck.java
+++ study2020/src/study2020/MemoryCheck.java
@@ -1,12 +1,12 @@
 package study2020;
 
-public class K1229 {
+public class MemoryCheck {
 
 		public static void main(String[] args) {
 			Runtime.getRuntime().gc(); 
 			// 가비지컬렉션으로 보다 더 정확하게 측정
 			long used = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-			System.out.println("used memory is " + used + " bytes");
+			System.out.println("used memory is" + used + " bytes");
 		}
 	}
 
