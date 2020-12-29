9186c8b843048d974c6efeb9884d624efb3f41e7
kinosaji <75092257+kinosaji@users.noreply.github.com>
2020-12-29 11:37:05 +0900

    사용중인 메모리확인이 필요할때

---
+++ study2020/src/study2020/K1229.java
@@ -0,0 +1,14 @@
+package study2020;
+
+public class K1229 {
+
+		public static void main(String[] args) {
+			Runtime.getRuntime().gc(); 
+			// 가비지컬렉션으로 보다 더 정확하게 측정
+			long used = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+			System.out.println("used memory is " + used + " bytes");
+		}
+	}
+
+	
+
